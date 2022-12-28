26648679f68c748033c361c72117f4d405729828
Luis Monroy <luis@luismonroy.me>
2022-12-28 11:02:33 -0500

    Unifying nav

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,12 @@
 // const year = document.getElementById('#current-year');
+// document.getElementById("current-year").innerHTML = new Date().getFullYear();
 
-document.getElementById("current-year").innerHTML = new Date().getFullYear();
+const menuEmail = document.querySelector (".navbar-email")
+const desktopMenu = document.querySelector (".desktop-menu")
+
+menuEmail.addEventListener("click", toggleDesktopMenu)
+
+function toggleDesktopMenu (){
+  desktopMenu.classList.toggle("inactive")
+}
+
