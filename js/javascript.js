5a8471744978a42e7f0c24aa4ee8737214f04779
Luis Monroy <luis@luismonroy.me>
2022-12-29 14:57:43 -0500

    Adjusting product detail and mobile

--- js/javascript.js
+++ js/javascript.js
@@ -2,13 +2,17 @@ const menuEmail = document.querySelector (".navbar-email");
 const desktopMenu = document.querySelector (".desktop-menu");
 const menuHamIcon = document.querySelector (".menu");
 const mobileMenu = document.querySelector (".mobile-menu");
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const menuCarritoIcon = document.querySelector (".navbar-shopping-cart");
 const shoppingCartContainer = document.querySelector ("#shoppingCartContainer");
+const productDetailContainer = document.querySelector ("#product-detail");
 const cardsContainer = document.querySelector (".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
+
 
 function toggleDesktopMenu (){
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -25,7 +29,7 @@ function toggleMobileMenu() {
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive'); 
   }
-  
+  closeProductDetailAside();
   mobileMenu.classList.toggle('inactive');
 }
 
@@ -35,9 +39,26 @@ function toggleCarritoAside() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive'); 
   }
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
   
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  }
+
   shoppingCartContainer.classList.toggle('inactive');
 }
+
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
+}
+
+
 
 const productList = [];
 productList.push({
@@ -79,6 +100,7 @@ function renderProducts(arr){
     
     const productImg = document.createElement("img");
     productImg.setAttribute("src", product.image);
+    productImg.addEventListener("click", openProductDetailAside);
     
     const productInfo = document.createElement ("div");
     productInfo.classList.add("product-info");
@@ -87,7 +109,6 @@ function renderProducts(arr){
     
     const productPrice = document.createElement ("p");
     productPrice.innerText ='$' + product.price;
-    
     const productName = document.createElement ("p");
     productName.innerText = product.name;
   
